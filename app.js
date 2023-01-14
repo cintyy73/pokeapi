c476c644c23058a8db8e4f15b723fb08fd9dea32
cintyy <cintiapaezdm@gmail.com>
2023-01-13 21:20:37 -0300

    ver error data

---
+++ app.js
@@ -0,0 +1,50 @@
+const $ = (selector) => document.querySelector(selector)
+
+const URL = "https://pokeapi.co/api/v2"
+const $input = $("#input")
+let pokemonSelect;
+const container = $(".card")
+const $btn = $("#btn") 
+
+const printData = (pokemonSelect = "") =>{
+fetch(`${URL}/pokemon/${pokemonSelect}`)
+.then((resp)=>resp.json())
+.then((data)=>{
+   console.log(data)})
+//    printPokemon(data)})
+
+// .catch((error)=>console.log(error))
+}
+// printData(pokemonSelect)
+printData("bulbasaur")
+
+
+printPokemon = () =>{
+  
+   container.innerHtml =`
+    <div class="card-image">
+    <figure class="image is-4by3">
+      <img  alt="Placeholder image">
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-left">
+        <figure class="image is-48x48">
+          <img src="${data.sprites.front_default}" alt="Placeholder image">
+        </figure>
+      </div>
+      <div class="media-content">
+        <p class="title is-4">${data.species.name}</p>
+        <p class="subtitle is-6"></p>
+      </div>
+    </div>  
+  </div>`
+
+}
+
+const search = () =>{
+    printData(pokemonSelect)
+    pokemonSelect = $input.value
+}
+$btn.addEventListener("click", search)
